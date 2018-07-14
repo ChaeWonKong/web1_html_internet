950fb14230aa7f678509548a07cd2c593e6d113e
공채원 <chaewonkong@gmail.com>
2018-07-14 12:18:17 -0700

    Change var to const and let

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
 function templateHTML(title, list, body) {
   return `
@@ -20,7 +20,7 @@ function templateHTML(title, list, body) {
 };
 
 function templateList(filelist){
-  var list = '<ol>';
+  let list = '<ol>';
     for (let i in filelist) {
       list += `<li><a href="?id=${filelist[i]}">${filelist[i]}</a></li>`;
     };
@@ -29,26 +29,26 @@ function templateList(filelist){
 };
 
 var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
+    const _url = request.url;
+    const queryData = url.parse(_url, true).query;
+    const pathname = url.parse(_url, true).pathname;
 
     if (pathname === '/') {
       if (queryData.id === undefined) {
         fs.readdir('./data', function(err, filelist) {
-          var title = 'Welcome';
-          var description = 'Hello, Node.js!';
-          var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+          const title = 'Welcome';
+          const description = 'Hello, Node.js!';
+          const list = templateList(filelist);
+          const template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
           response.writeHead(200);
           response.end(template);
         });
       } else {
         fs.readdir('./data', function(err, filelist) {
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-          var title = queryData.id;
-          var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+          const title = queryData.id;
+          const list = templateList(filelist);
+          const template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
           response.writeHead(200);
           response.end(template);
           });
