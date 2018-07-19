92759088b08676ab55366c388eacb23bcad25ec3
공채원 <chaewonkong@gmail.com>
2018-07-19 00:23:35 -0700

    Modify content

---
+++ syntax/callback.js
@@ -0,0 +1,17 @@
+// function a(){
+// 	console.log('A');
+// };
+
+// a();
+
+var a = function(){
+	console.log('A')
+};
+console.log('No results')
+/*
+function slowfunc(callback){
+	callback();
+};
+
+slowfunc(a);
+*/
