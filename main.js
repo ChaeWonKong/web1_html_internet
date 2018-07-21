2a2b9dc45f2db8fac42bc000aa1500bf19333599
공채원 <chaewonkong@gmail.com>
2018-07-21 13:08:05 -0700

    Add update and delete function

--- main.js
+++ main.js
@@ -56,7 +56,14 @@ var app = http.createServer(function(request,response){
           const list = templateList(filelist);
           const template = templateHTML(title, list,
             `<h2>${title}</h2><p>${description}</p>`,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            `
+              <a href="/create">create</a>
+              <a href="/update?id=${title}">update</a>
+              <form action="/delete_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <button type="submit">delete</button>
+              </form>
+            `
             );
           response.writeHead(200);
           response.end(template);
@@ -78,7 +85,6 @@ var app = http.createServer(function(request,response){
           response.writeHead(200);
           response.end(template);
         });
-
 
     } else if(pathname === '/create_process'){
       let body = '';
@@ -109,9 +115,40 @@ var app = http.createServer(function(request,response){
             </form> 
           `,
           `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-          );
-        response.writeHead(200);
-        response.end(template);
+        );
+      response.writeHead(200);
+      response.end(template);
+      });
+    });
+    } else if(pathname === '/update_process'){
+      let body = '';
+      request.on('data', function(data){
+        body += data;
+      });
+      request.on('end', function(){
+        const post = qs.parse(body);
+        const title = post.title;
+        const id = post.id;
+        const description = post.description;
+        console.log('id: ' + id + ' Title: ' + title);
+        fs.rename(`./data/${id}`, `./data/${title}`, function(err){
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          response.writeHead(302, {Location: `/?id=${title}`});
+          response.end();
+          });
+        });
+      });
+    } else if(pathname === '/delete_process'){
+      let body = '';
+      request.on('data', function(data){
+        body += data;
+      });
+      request.on('end', function(){
+        const post = qs.parse(body);
+        const id = post.id;
+        fs.unlink(`./data/${id}`, function(err){
+          response.writeHead(302, {Location: `/`});
+          response.end();
         });
       });
     }
