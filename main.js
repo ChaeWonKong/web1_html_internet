1fae8894afeb99e9657eb8d3530fabaeb2473a42
공채원 <chaewonkong@gmail.com>
2018-07-29 19:25:49 -0700

    Code refactoring

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const url = require('url');
 const qs = require('querystring');
 const path = require('path');
 const sanitizeHtml = require('sanitize-html');
-
 const template = require('./lib/template.js');
 
 var app = http.createServer(function(request,response){
